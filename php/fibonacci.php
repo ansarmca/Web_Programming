6913289e5b8439c247224ed8044f0a38b75e4992
ansarmca <ansarkp300@gmail.com>
2024-12-09 14:28:46 +0530

    Add files via upload

---
+++ php/fibonacci.php
@@ -0,0 +1,13 @@
+<?php
+function fibonacci($n) {
+    if ($n <= 1) {
+        return $n;
+    } else {
+        return fibonacci($n - 1) + fibonacci($n - 2);
+    }
+}
+
+echo "Fibonacci Series: ";
+for ($i = 0; $i <= 10; $i++) {
+    echo fibonacci($i) . " ";
+}
